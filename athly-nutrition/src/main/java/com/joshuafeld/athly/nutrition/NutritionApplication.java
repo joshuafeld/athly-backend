6281dd8efed04df02b38c236f698a75bd693bf10
joshuafeld <mail@joshuafeld.com>
2025-05-07 02:05:39 +0200

    Setup basic working config and gateway server

---
+++ athly-nutrition/src/main/java/com/joshuafeld/athly/nutrition/NutritionApplication.java
@@ -0,0 +1,12 @@
+package com.joshuafeld.athly.nutrition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutritionApplication.class, args);
+    }
+}
