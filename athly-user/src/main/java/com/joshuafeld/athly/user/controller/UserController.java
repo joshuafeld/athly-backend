52b16b78dcc5133a1706b1eac52944453d4a4cea
joshuafeld <mail@joshuafeld.com>
2025-05-19 22:44:29 +0200

    Change the service predicates

--- athly-user/src/main/java/com/joshuafeld/athly/user/controller/UserController.java
+++ athly-user/src/main/java/com/joshuafeld/athly/user/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +22,7 @@ import java.util.List;
  * A user controller.
  */
 @RestController
+@RequestMapping("/users")
 public final class UserController {
 
     private final UserService service;
